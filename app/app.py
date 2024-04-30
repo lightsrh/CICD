004185b18769520b0ae824aa3e987b8995e102c3
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-04-30 16:18:08 +0200

    add test post

--- app/app.py
+++ app/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
+
+@app.post('/city')
+def createCity():
+    data = request.json
+    print(data.get('id'))
+    print(data.get('department_code'))
+    print(data.get('insee_code'))
+    print(data.get('zip_code'))
+    print(data.get('name'))
+    print(data.get('lat'))
+    print(data.get('lon'))
+    return data, 201
 
 @app.route("/")
 def hello_world():
--- app/test_app.py
+++ app/test_app.py
@@ -19,7 +19,26 @@ class AppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'<p>Hello, World!</p>')
     
-
+    def test_create_city(self):
+        response = self.app.post('/city', json={
+            "id": 1,
+            "department_code": "75",
+            "insee_code": "75056",
+            "zip_code": "75000",
+            "name": "Paris",
+            "lat": 48.8566,
+            "lon": 2.3522
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json, {
+            "id": 1,
+            "department_code": "75",
+            "insee_code": "75056",
+            "zip_code": "75000",
+            "name": "Paris",
+            "lat": 48.8566,
+            "lon": 2.3522
+        })
 
 
 
