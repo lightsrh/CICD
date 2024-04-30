2f0578c056df8e454f517685c7b19993241e45c9
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-04-30 15:33:14 +0200

    Add .env file and docker-entrypoint-initdb.d/init.sql for PostgreSQL configuration

---
+++ dataset/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE city_id_seq;
+
+CREATE TABLE city (
+    id INT NOT NULL  PRIMARY KEY,
+    department_code VARCHAR(255) NOT NULL,
+    insee_code VARCHAR(255),
+    zip_code VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    lat FLOAT NOT NULL,
+    lon FLOAT NOT NULL
+);
