dd48700c215d4132a5e5e799f9e1eeaf17116155
giada-de-martino <giada.de-martino@etu.umontpellier.fr>
2024-06-03 13:36:25 +0200

    test ci 3

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-source .env
-
 
 echo "POSTGRES_HOST: $POSTGRES_HOST, POSTGRES_PORT: $POSTGRES_PORT, POSTGRES_DB: $POSTGRES_DB, POSTGRES_USER: $POSTGRES_USER, POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
-#créer .pgpass
+
+# Create .pgpass
 echo "$POSTGRES_HOST:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD" > ~/.pgpass
 chmod 600 ~/.pgpass
 cat ~/.pgpass
 
+# Uncomment these lines if you are using Docker Compose to start the PostgreSQL database
 # docker compose down --remove-orphans
 # docker compose up -d
 
