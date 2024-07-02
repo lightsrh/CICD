21c11968d5dcc4842cb303fad52fa0c24f3307c6
giada-de-martino <giada.de-martino@etu.umontpellier.fr>
2024-07-02 20:55:59 +0200

    test ci 13

--- init_db.py
+++ init_db.py
@@ -6,21 +6,25 @@ from dotenv import load_dotenv
 def create_sequence(conn, cur):
     # SQL CREATE SEQUENCE statement
     sql = "CREATE SEQUENCE IF NOT EXISTS city_id_seq"
-
     # Execute the SQL statement
     cur.execute(sql)
-
     # Commit the changes
     conn.commit()
 
 def create_table(conn, cur):
-
     # SQL CREATE TABLE statement
-    sql = "CREATE TABLE IF NOT EXISTS city ( id SERIAL PRIMARY KEY, department_code VARCHAR(255) NOT NULL, insee_code VARCHAR(255), zip_code VARCHAR(255), name VARCHAR(255) NOT NULL, lat FLOAT, lon FLOAT)"
-
+    sql = """
+    CREATE TABLE IF NOT EXISTS city (
+        id SERIAL PRIMARY KEY,
+        department_code VARCHAR(255) NOT NULL,
+        insee_code VARCHAR(255),
+        zip_code VARCHAR(255),
+        name VARCHAR(255) NOT NULL,
+        lat FLOAT,
+        lon FLOAT
+    )"""
     # Execute the SQL statement
     cur.execute(sql)
-
     # Commit the changes
     conn.commit()
 
@@ -34,11 +38,15 @@ def insert_data(conn, cur, data):
         lon = record["lon"]
 
         # SQL INSERT statement
-        sql = "INSERT INTO city (id, department_code, insee_code, zip_code, name, lat, lon) VALUES ((SELECT nextval('city_id_seq')), %s, %s, %s, %s, %s, %s)"
+        sql = """
+        INSERT INTO city (id, department_code, insee_code, zip_code, name, lat, lon) 
+        VALUES ((SELECT nextval('city_id_seq')), %s, %s, %s, %s, %s, %s)
+        """
         values = (department_code, insee_code, zip_code, name, lat, lon)
-
         # Execute the SQL statement
         cur.execute(sql, values)
+
+# Load environment variables
 load_dotenv()
 
 conn = psycopg2.connect(
@@ -51,16 +59,22 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-create_sequence(conn, cur)
+# # Drop the existing table and sequence to ensure a clean state
+# cur.execute("DROP TABLE IF EXISTS city")
+# cur.execute("DROP SEQUENCE IF EXISTS city_id_seq")
 
+# Create sequence and table
+create_sequence(conn, cur)
 create_table(conn, cur)
 
+# Load and insert data from JSON
 with open("cities.json", "r") as json_file:
     data = json.load(json_file)
-
 insert_data(conn, cur, data)
 
+# Commit the changes
 conn.commit()
 
+# Close the cursor and connection
 cur.close()
 conn.close()
